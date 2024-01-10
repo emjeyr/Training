051279756b795d4e1056b1898c3099212ab108cd
Manoj R <manojrgowda18@gmail.com>
2024-01-10 17:12:20 +0530

    Java code for number game

---
+++ number_game.java
@@ -0,0 +1,36 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int lowerBound = 1;
+        int upperBound = 100;
+        int secretNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+        int guess;
+        int attempts = 0;
+
+        System.out.println("Welcome to the Number Game!");
+        System.out.println("I've selected a number between " + lowerBound + " and " + upperBound);
+
+        do {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < secretNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > secretNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+            }
+        } while (guess != secretNumber);
+
+        scanner.close();
+    }
+}
+
